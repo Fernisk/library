3bd73b81e8587570172bcd1e1653a92b0f67d766
Fernisk <miyaresf90@gmail.com>
2022-01-20 12:50:07 -0500

    added form, still more logic to be done

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-
+//dom elements
+const body = document.querySelector("body");
+const booksContainer = document.querySelector(".books-container");
+const addButton = document.querySelector(".addButton");
+const submitButton = document.querySelector(".btn");
+const form = document.querySelector("#containerForm");
+const popupForm = document.querySelector(".formPopup");
 //array of objects container 
 let myLibrary = [];
 
@@ -13,17 +19,47 @@ function Book (title, author, pages, readOrNot) {
     }
 
 }
-function openTheForm() {
-  document.querySelector(".popupForm").style.display = "block";
-}
 
 function addBookToLibrary() {
 
 }
 
+//will set ids for us
+let id = (id) => document.getElementById(id);
+
+//sets values with their key on the form
+let title = id("title"),
+    author = id("author"),
+    pages = id("pages"),
+    checkRead = id("checkRead");
+
+
+function openForm() {
+   booksContainer.style.opacity = "0.3";
+   popupForm.style.display = "block";
+}
+
+
+const submitForm = () => {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        form.reset()
+         popupForm.style.display = "none";
+         booksContainer.style.opacity = "1";
+        console.log(form.elements.title.value);
+    })
+}
+
 function deleteBook() {
 
 }
+
+//opens the form
+addButton.addEventListener("click", openForm);
+//closes the form / will also return all the values
+submitButton.addEventListener("click", submitForm);
+
+
 
 
 
